fe2d8bd44478fe63d77d979a3d4a910516b96058
maakasamindian <74304156+maakasamindian@users.noreply.github.com>
2021-09-21 15:38:43 +0530

    Add files via upload

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,4 +1,3 @@
-document.addEventListener('contextmenu', event => event.preventDefault());
 
 const filterImg = document.querySelectorAll(".pre, .inner-area");
 
@@ -19,8 +18,9 @@ shadow = document.querySelector(".shadow");
 function preview(element){
   //once user click on any image then remove the scroll bar of the body, so user can't scroll up or down
   document.querySelector("body").style.overflow = "hidden";
+ 
   let selectedPrevImg = element.querySelector("img").src; //getting user clicked image source link and stored in a variable
-  let selectedImgCategory = element.getAttribute("data-name"); //getting user clicked image data-name value
+   let selectedImgCategory = element.getAttribute("data-name"); //getting user clicked image data-name value
   previewImg.src = selectedPrevImg; //passing the user clicked image source in preview image source
   categoryName.textContent = selectedImgCategory; //passing user clicked data-name value in category name
   previewBox.classList.add("show"); //show the preview image box
@@ -29,7 +29,157 @@ function preview(element){
     previewBox.classList.remove("show"); //hide the preview box
     shadow.classList.remove("show"); //hide the light grey background
     document.querySelector("body").style.overflow = "auto"; //show the scroll bar on body
-  }
+}
+}
+var cursor = {
+    delay: 8,
+    _x: 0,
+    _y: 0,
+    endX: (window.innerWidth / 2),
+    endY: (window.innerHeight / 2),
+    cursorVisible: true,
+    cursorEnlarged: false,
+    $dot: document.querySelector('.cursor-dot'),
+    $outline: document.querySelector('.cursor-dot-outline'),
+    
+    init: function() {
+        // Set up element sizes
+        this.dotSize = this.$dot.offsetWidth;
+        this.outlineSize = this.$outline.offsetWidth;
+        
+        this.setupEventListeners();
+        this.animateDotOutline();
+    },
+    
+//     updateCursor: function(e) {
+//         var self = this;
+        
+//         console.log(e)
+        
+//         // Show the cursor
+//         self.cursorVisible = true;
+//         self.toggleCursorVisibility();
+
+//         // Position the dot
+//         self.endX = e.pageX;
+//         self.endY = e.pageY;
+//         self.$dot.style.top = self.endY + 'px';
+//         self.$dot.style.left = self.endX + 'px';
+//     },
+    
+    setupEventListeners: function() {
+        var self = this;
+        
+        // Anchor hovering
+        document.querySelectorAll('a').forEach(function(el) {
+            el.addEventListener('mouseover', function() {
+                self.cursorEnlarged = true;
+                self.toggleCursorSize();
+            });
+            el.addEventListener('mouseout', function() {
+                self.cursorEnlarged = false;
+                self.toggleCursorSize();
+            });
+        });
+        
+        // Click events
+        document.addEventListener('mousedown', function() {
+            self.cursorEnlarged = true;
+            self.toggleCursorSize();
+        });
+        document.addEventListener('mouseup', function() {
+            self.cursorEnlarged = false;
+            self.toggleCursorSize();
+        });
+  
+  
+        document.addEventListener('mousemove', function(e) {
+            // Show the cursor
+            self.cursorVisible = true;
+            self.toggleCursorVisibility();
+
+            // Position the dot
+            self.endX = e.pageX;
+            self.endY = e.pageY;
+            self.$dot.style.top = self.endY + 'px';
+            self.$dot.style.left = self.endX + 'px';
+        });
+        
+        // Hide/show cursor
+        document.addEventListener('mouseenter', function(e) {
+            self.cursorVisible = true;
+            self.toggleCursorVisibility();
+            self.$dot.style.opacity = 1;
+            self.$outline.style.opacity = 1;
+        });
+        
+        document.addEventListener('mouseleave', function(e) {
+            self.cursorVisible = true;
+            self.toggleCursorVisibility();
+            self.$dot.style.opacity = 0;
+            self.$outline.style.opacity = 0;
+        });
+    },
+    
+    animateDotOutline: function() {
+        var self = this;
+        
+        self._x += (self.endX - self._x) / self.delay;
+        self._y += (self.endY - self._y) / self.delay;
+        self.$outline.style.top = self._y + 'px';
+        self.$outline.style.left = self._x + 'px';
+        
+        requestAnimationFrame(this.animateDotOutline.bind(self));
+    },
+    
+    toggleCursorSize: function() {
+        var self = this;
+        
+        if (self.cursorEnlarged) {
+            self.$dot.style.transform = 'translate(-50%, -50%) scale(0.75)';
+            self.$outline.style.transform = 'translate(-50%, -50%) scale(1.5)';
+        } else {
+            self.$dot.style.transform = 'translate(-50%, -50%) scale(1)';
+            self.$outline.style.transform = 'translate(-50%, -50%) scale(1)';
+        }
+    },
+    
+    toggleCursorVisibility: function() {
+        var self = this;
+        
+        if (self.cursorVisible) {
+            self.$dot.style.opacity = 1;
+            self.$outline.style.opacity = 1;
+        } else {
+            self.$dot.style.opacity = 0;
+            self.$outline.style.opacity = 0;
+        }
+    }
 }
 
+cursor.init();
+var menu = document.querySelector('.menu');
+
+function showMenu(x, y){
+    menu.style.left = x - 'px';
+    menu.style.top = y - 'px';
+    menu.classList.add('menu-show');
+}
+
+function hideMenu(){
+    menu.classList.remove('menu-show');
+}
+
+function onContextMenu(e){
+    e.preventDefault();
+    showMenu(e.pageX, e.pageY);
+    document.addEventListener('mousedown', onMouseDown, false);
+}
+
+function onMouseDown(e){
+    hideMenu();
+    document.removeEventListener('mousedown', onMouseDown);
+}
+
+document.addEventListener('contextmenu', onContextMenu, false);
 
